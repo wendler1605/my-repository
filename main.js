764db53433878e732b9ec069f50a2d28480655d1
wendler1605 <160506472+wendler1605@users.noreply.github.com>
2024-05-27 11:43:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
