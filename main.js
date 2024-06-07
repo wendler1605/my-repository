6c16a9ab3e89d3f2b039159c7c1cdf58ef44a66c
wendler1605 <160506472+wendler1605@users.noreply.github.com>
2024-06-07 09:48:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 const numeroSenha = document.querySelector('.parametro-senha_texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LetrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = docoument.querySelectorAll('.parametro-senha_botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes [0].onclick = diminuiTamanho
+botoes [1].onclick = aumentaTamanho
+
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha
