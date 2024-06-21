9bc73c2a9aae7e515275a5954c3045378392cc1b
wendler1605 <160506472+wendler1605@users.noreply.github.com>
2024-06-21 09:41:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -58,3 +58,20 @@ function geraSenha() {
     }
     campoSenha.value = senha;
     clasificaSenha(alfabeto.length);
+
+}
+
+function clasificaSenha(tamanhoAlfabeto){
+    let entropia = tamanohSenha * Math.log2(tamanhoAlfbeto);
+    console.log(entropia);
+    forcaSenha.classList.remove('fraca','media','forte');
+    if (entropia > 57){
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
+    }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "um computador pode levar ate = + math.floor"(2**entropia/(100e6*60*60*24)) + dias p
+}
